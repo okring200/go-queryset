128252319dc1eca5f02411af78b6d879f2e97196
okring200 <okring200@gmail.com>
2021-09-12 03:18:30 +0900

    Change go module name

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/jirfag/go-queryset
+module github.com/okring200/go-queryset
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- internal/queryset/methods/context.go
+++ internal/queryset/methods/context.go
@@ -1,8 +1,8 @@
 package methods
 
 import (
-	"github.com/jirfag/go-queryset/internal/parser"
-	"github.com/jirfag/go-queryset/internal/queryset/field"
+	"github.com/okring200/go-queryset/internal/parser"
+	"github.com/okring200/go-queryset/internal/queryset/field"
 )
 
 type QsStructContext struct {
